d0ea07d4500f55f5281e0b71ef6bdbcf304377f3
Guilherme Almeida <guilam.dev@gmail.com>
2025-01-29 13:21:55 -0300

    initialize serial communication demo with serialport library

---
+++ index.js
@@ -0,0 +1,23 @@
+import { SerialPort } from "serialport";
+import { ReadlineParser } from "@serialport/parser-readline";
+import path from "path";
+
+const myPort = new SerialPort({
+  path: "/dev/ttyACM0",
+  baudRate: 9600,
+  autoOpen: true,
+});
+
+console.log("Serial port is open");
+
+const parser = new ReadlineParser();
+myPort.pipe(parser);
+
+parser.on("data", (line) => { console.log(line); });
+
+let i = 10;
+while (i > -1) {
+  myPort.write(`<${i}>`);
+  console.log(`Sent: <${i}>`);
+  i--;
+}
